3c4b6498885d37ff3107a69223aff74e73961c7d
Solidshots <seanbrent5@gmail.com>
2021-07-01 13:46:27 -0400

    form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,43 @@
 import {useState} from 'react';
+import {validateEmail} from '../../utils/helpers';
 
 function ContactForm(){
 
     //define the initial form state and set form state function
     const [formState, setFormState] = useState({name: '', email: '', message: ''});
+    const [errorMessage, setErrorMessage] = useState('');
 
     //destructure the form components from form state
     const {name, email, message} = formState;
 
     //Called whenever there is a change to the input areas
     function handleChange(event){
-        setFormState({...formState, [event.target.name]: event.target.value})
+        if(event.target.name === 'email'){
+            const isValid = validateEmail(event.target.value);
+
+            if(!isValid)
+                setErrorMessage('Your email is invalid.');
+            else
+                setErrorMessage('');
+        }
+        else{
+            if(!event.target.value.length)
+                setErrorMessage(`${event.target.name} is a required field.`);
+            else
+                setErrorMessage('');
+        }
+
+        if (!errorMessage) 
+            setFormState({ ...formState, [event.target.name]: event.target.value });
     }
 
     //Called when the form is submitted
     function handleSubmit(event){
         event.preventDefault();
         //****Back-end team to complete***
-        console.log(formState);
+        if(name && email && message){
+            console.log(formState);
+        }
     }
 
     return (
@@ -26,16 +46,21 @@ function ContactForm(){
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message: </label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+                { errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
